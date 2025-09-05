c262f49bc8db406d1824bf02d75081fe0d31e8f5
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-09-05 17:15:10 +0200

    [UPD] snack 1

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,20 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+// Hai ricevuto un dato generico da un'API, ma non sai di che tipo sia… Il tuo compito è controllare il tipo del dato e stampare il valore in modo corretto.
+// Se è una stringa: stampala in maiuscolo
+// Se è un numero: moltiplicalo per due e stampalo
+// Se è un booleano: stampa “Sì” o “No” in base al suo valore
+// In tutti gli altri casi: stampa “Tipo non supportato”
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+let dato: unknown;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+if (typeof dato === 'string') {
+  console.log(dato.toUpperCase())
+}
+else if (typeof dato === 'number') {
+  console.log(dato * 2)
+}
+else if (typeof dato === 'boolean') {
+  console.log(dato ? 'Sì' : 'No')
+}
+else {
+  console.log('Tipo non supportato')
+}
